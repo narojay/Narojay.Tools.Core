d4420cae7217d064e63235a184aaed1c45fdb3e2
narojay <hj200812@126.com>
2021-10-07 02:34:18 +0800

    Feature Md5加密

---
+++ Security/Encrypt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Narojay.Tools.Core.Security
+{
+    public static class Encrypt
+    {
+        public static string Salt = "narojay";
+
+        public static string Md5Encrypt(string message)
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            byte[] bytes = md5.ComputeHash(buffer);
+
+            return bytes.Aggregate("", (current, item) => current + item.ToString("x2"));
+
+        }
+
+        public static string Md5Encrypt(string message, string salt)
+        {
+            var value = message;
+            value += salt == string.Empty ? Salt : salt;
+            using MD5 md5 = MD5.Create();
+            byte[] buffer = Encoding.UTF8.GetBytes(value);
+            byte[] bytes = md5.ComputeHash(buffer);
+            return bytes.Aggregate("", (current, item) => current + item.ToString("x2"));
+        }
+    }
+}
